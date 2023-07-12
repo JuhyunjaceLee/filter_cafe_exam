acf2a3592a73f004f2b66b398eae1022dcd962f5
JuhyunjaceLee <heyjace777@gmail.com>
2023-07-13 06:51:00 +0900

    login func with cookeis

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,41 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./LoginPage.module.css";
 import { BsFillCaretRightFill } from "react-icons/bs";
 import SocialLoginBtns from "../components/LoginComponents/SocialLoginBtns";
+import { useCookies } from "react-cookie";
 
 export default function LoginPage() {
-  const [checked, setChecked] = useState(false);
   const [userId, setUserId] = useState("");
-  const saveUserId = (e) => {
-    setUserId(e.target.value);
-  };
+  const [cookies, setCookie, removeCookie] = useCookies(["rememberUserId"]);
+  const [checked, setChecked] = useState(false);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    if (checked) {
-      save();
-      createLocalList();
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  // };
+
+  useEffect(() => {
+    if (cookies.rememberUserId !== undefined) {
+      setUserId(cookies.rememberUserId);
+      setChecked(true);
+    }
+  }, []);
+
+  const handleOnChange = (e) => {
+    setChecked(e.target.checked);
+    if (!e.target.checked) {
+      removeCookie("rememberUserId");
     }
   };
-
-  const checkedHandle = (e) => {
-    const checkedbox = e.target.checked;
-    if (checkedbox) {
-      setChecked(!checked);
-    }
-  };
-
-  const auths = [];
-  const save = () => {
-    localStorage.setItem("auths", JSON.stringify(auths));
-  };
-  const createLocalList = () => {
-    const auth = {
-      authId: userId,
-    };
-    auths.push(auth);
-  };
-
   return (
     <>
       <div className={style.section}>
@@ -50,8 +40,8 @@ export default function LoginPage() {
             className={style.input}
             type="text"
             placeholder="Email"
-            value={userId}
-            onChange={saveUserId}
+            defaultValue={userId}
+            onChange={(e) => setUserId(e.target.value)}
           />
           <input
             className={style.input}
@@ -62,8 +52,8 @@ export default function LoginPage() {
             <input
               id="check"
               type="checkbox"
-              onClick={checkedHandle}
-              readOnly
+              onChange={(e) => handleOnChange(e)}
+              checked={checked}
             />
             <label htmlFor="check">Remember Me</label>
           </fieldset>
@@ -71,11 +61,7 @@ export default function LoginPage() {
             <BsFillCaretRightFill />
             Forgot password?
           </span>
-          <button
-            className={style.login_btn}
-            type="submit"
-            onClick={handleLogin}
-          >
+          <button className={style.login_btn} type="submit">
             LOGIN
           </button>
         </form>
